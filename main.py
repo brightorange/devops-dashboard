e3358436b1b60176986d9d466991df2d5f51dd19
Inga Kaydanova <inga.kaidanova@jobleads.com>
2026-03-22 22:29:33 +0200

    Implement initial menu for the application

---
+++ main.py
@@ -0,0 +1,17 @@
+while True:
+    input = input(f"""Please select an option:
+– [1] System Info
+– [2] Log Checker
+– [3] Task List
+– [0] Exit
+    """)
+    if input == "0":
+        break;
+    elif input == "1":
+        print("Coming soon...")
+    elif input == "2":
+        print("Coming soon..")
+    elif input == "3":
+        print("Coming soon...")
+    else:
+        print("Invalid option")
